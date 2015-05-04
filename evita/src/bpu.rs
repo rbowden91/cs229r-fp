247d60277201161ed749d328bd1b00a47f5e96ae
Oreoluwa Babarinsa <oreoluwa.babarinsa@gmail.com>
2015-05-04 15:30:27 -0400

    Merged BPU files

--- evita/src/bpu.rs
+++ evita/src/bpu.rs
@@ -1,1 +1,57 @@
-//Instructions.rs - contains and describes the instruction set for Evita
+//bpu.rs - defines the functionality of a biological processing unit
+
+//The registers which exist on our BPU
+enum Register {
+    //AX register
+    AX,
+    //BX register, our default register
+    BX,
+    //CX register
+    CX
+}
+
+//The various heads which shift through our tape
+enum Head {
+    //Instruction Pointer
+    IP,
+    //Read head (i.e. where on the tape are we reading?)
+    RH,
+    //Write head (i.e. where on the tap are we writing?)
+    WH,
+    //Floating head, used for copying the state of the tape
+    FH
+}
+
+
+//Implementation of convenience methods for registers
+impl Register {
+    //Matches each register to their complement in Evita
+    fn getComplement(&self) -> Register {
+        match *self {
+            AX => BX,
+            BX => CX,
+            CX => AX
+        }
+    }
+    //Maps the active register onto a given head
+    fn resolveHead(&self) -> Head {
+        match *self {
+            AX => IP,
+            BX => RH,
+            CX => WH
+        }
+    }
+}
+
+//The InstructionSet which is run by our BPU
+enum InstructionSet {
+    //A NO-OP which sets the target register to the value it contains
+    NOP(Register),
+    //Conditionally jumps if CURR_REG != COMP_REG
+    IFNEQU,
+    //Conditionally jumps if CURR_REG < COMP_REG
+    IFLESS,
+
+
+
+}
